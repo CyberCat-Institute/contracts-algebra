bdba0bcdc68800c250a7e9e60a90a109994dc00f
Philipp Zahn <philipp.zahn@protonmail.com>
2022-10-15 07:34:19 -0500

    Composed clause inspection and warranty

--- contracts-open-games/src/Contracts/ComposedClauses/ComposedClauses.hs
+++ contracts-open-games/src/Contracts/ComposedClauses/ComposedClauses.hs
@@ -9,14 +9,29 @@
 module Contracts.ComposedClauses.ComposedClauses
   where
 
+import Contracts.ShipmentAndDelivery.InspectionClauseDecisions
 import Contracts.ShipmentAndDelivery.ShipmentLocationClauses
 import Contracts.ShipmentAndDelivery.RiskOfLossClauses
 import Contracts.ShipmentAndDelivery.Types
+import Contracts.Warranty.Clauses
 import Contracts.Types
 import Engine.Engine
 import Preprocessor.Preprocessor
 
 
+-----------------------------
+-- 0. Auxiliary functionality
+----------------------------
+
+warrantyApplies :: (Bool,Costs) -> Costs
+warrantyApplies (True,c)  = c
+warrantyApplies (False,_) = 0
+
+----------------------
+-- 1. Composed clauses
+----------------------
+
+-- | Compose riskOfLoss contract and shipmentCosts clauses
 locationPlusRiskOfLoss seller buyer damageFunction mode costFunction = [opengame|
 
     inputs    : damage, location, buyerReceived ;
@@ -34,6 +49,40 @@ locationPlusRiskOfLoss seller buyer damageFunction mode costFunction = [opengame
     inputs    : location ;
     feedback  : ;
     operation : shipmentCosts seller buyer costFunction ;
+    outputs   : costsSeller,costsBuyer;
+    returns   :  ;
+
+    :-----:
+
+    outputs   : ;
+    returns   : ;
+|]
+
+-- | Compose warranty and  
+inspectionPlusWarranty seller buyer  daysThreshold inspectionCondition warrantyCostFunction warrantyAffected = [opengame|
+
+    inputs    : daysSinceShipment, warranty ;
+    feedback  : ;
+
+    :-----:
+
+    inputs    : daysSinceShipment;
+    feedback  : ;
+    operation : inspectionDecision seller buyer  daysThreshold inspectionCondition ;
+    outputs   : nonConfirming;
+    returns   : costsSeller,costsBuyer ;
+
+    inputs    : nonConfirming, warranty;
+    feedback  : ;
+    operation : forwardFunction $ warrantyApplies ;
+    outputs   : warrantyRealized;
+    returns   : ;
+
+   
+
+    inputs    : warrantyRealized ;
+    feedback  : ;
+    operation : warrantyCosts seller buyer warrantyCostFunction ;
     outputs   : costsSeller,costsBuyer;
     returns   :  ;
 
