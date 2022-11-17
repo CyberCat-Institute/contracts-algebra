653c84d72f2b6819c147554717cd007041c4e05d
lndavis6 <lancendavis47@gmail.com>
2022-11-17 16:04:51 -0500

    new acceptance decision in SAFE

---
+++ contracts-open-games/src/Contracts/Safe.hs
@@ -0,0 +1,11 @@
+-- Imports here
+
+------------------------------------------------------------------------------
+
+---------------
+-- 0 Data types
+---------------
+
+-- Acceptance decision
+data Acceptance = Accept | Reject
+  deriving (Show, Eq, Ord)
