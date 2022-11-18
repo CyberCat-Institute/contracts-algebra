d7ec1fef6ac1e330ad8c1ab7b5419b26bf40c82a
lndavis6 <lancendavis47@gmail.com>
2022-11-18 14:47:03 -0500

    added one payoff

--- contracts-open-games/src/Contracts/Safe.hs
+++ contracts-open-games/src/Contracts/Safe.hs
@@ -1,11 +1,36 @@
 -- Imports here
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TupleSections #-}
 
 ------------------------------------------------------------------------------
+module Contracts.Safe where
+
+import Engine.Engine
+import Preprocessor.Preprocessor
 
 ---------------
 -- 0 Data types
+-- Players: Investor, Company
+-- Actions: cap is set to a certain percentage, then the investor and company can choose to settle or not settle
 ---------------
+type Actions = Int
+
+-- Action space
+type ActionSpace = [Action]
 
 -- Acceptance decision
 data Acceptance = Accept | Reject
   deriving (Show, Eq, Ord)
+
+------------
+-- 1 Payoffs
+-- The payoff is the amount of cap table space that the
+------------
+data SafeMove = Settle | DontSettle deriving (Eq, Ord, Show)
