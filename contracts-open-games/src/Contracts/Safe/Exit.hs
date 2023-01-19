4d19bcaf380b5d74dd342a52d13be41ef1c5abb6
lndavis6 <lancendavis47@gmail.com>
2023-01-19 14:27:28 -0500

    fixed exit game

--- contracts-open-games/src/Contracts/Safe/Exit.hs
+++ contracts-open-games/src/Contracts/Safe/Exit.hs
@@ -7,7 +7,13 @@ import Preprocessor.Preprocessor
 -- exit or no?
 data ExitType = IPO | Merger | Dissolution deriving (Show, Eq, Ord)
 
-exitDecision ExitType =
+-- | Payoff matrix for player i given i's action and j's action
+exitMatrix :: ExitType -> Double -> Double
+exitMatrix IPO x = (^) x 2 -- x^2 for exponential gains as the public company grows more investors and returns over time
+exitMatrix Merger x = ((**) (x * 0.3) 2) + 60000 -- decaying reward over time due to the instant nature of a merger
+exitMatrix Dissolution x = 0 * x -- Everyone loses in a dissolution
+
+exitDecision ExitType valuation=
   [opengame|
         inputs    : CapTable;
         feedback  : ;
@@ -16,7 +22,7 @@ exitDecision ExitType =
 
         inputs    : CapTable;
         feedback  : ;
-        operation : // forward function?
+        operation : forwardFunction exitMatrix ExitType valuation;
         outputs   : [StockValPair] ;
         returns   : ;
 
