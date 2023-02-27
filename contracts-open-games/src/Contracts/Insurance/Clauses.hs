4e25901f6d2eb720a05b19582810584d2d395642
Philipp Zahn <philippzahn@Philipps-MBP.fritz.box>
2023-02-27 11:21:26 +0100

    Added contract clause

---
+++ contracts-open-games/src/Contracts/Insurance/Clauses.hs
@@ -0,0 +1,115 @@
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances, FlexibleContexts, TemplateHaskell #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE QuasiQuotes #-}
+
+module Contracts.Insurance.Clauses where
+
+import OpenGames.Engine.Engine
+import OpenGames.Preprocessor
+
+import Contracts.Types
+-- Insurance arrangements
+
+
+---------------
+-- 0 Data types
+---------------
+
+data Insuree = Seller | Buyer | Both
+  deriving (Show,Eq,Ord)
+
+type InsuranceTime = Int
+type Time = Int
+
+favorability :: (Insuree, SellerCosts, BuyerCosts, InsuranceTime, Time) -> (SellerCosts,BuyerCosts)
+favorability (insuree,costsSeller,costsBuyer,deadline,time)
+ | insuree == Seller && time < deadline = (costsSeller,0)
+ | insuree == Buyer && time < deadline = (0,costsBuyer)
+ | insuree == Both && time < deadline = (costsSeller,costsBuyer)
+ | otherwise = (0,0)
+
+--------------------
+-- 1 Insurance clauses
+--------------------
+
+-- | Insurance in place for a period of time
+insuranceClause seller buyer insuranceCostFunction = [opengame|
+
+    inputs    : insuranceCondition, amountSeller,amountBuyer, deadline, time;
+    feedback  : ;
+
+    :-----:
+
+    inputs    : insuranceCondition, amountSeller,amountBuyer, deadline, time ;
+    feedback  : ;
+    operation : forwardFunction $ insuranceCostFunction;
+    outputs   : costsSeller,costsBuyer ;
+    returns   : ;
+
+
+    inputs    : costsSeller;
+    feedback  : ;
+    operation : addPayoffs seller ;
+    outputs   :  ;
+    returns   : ;
+
+
+    inputs    : costsBuyer ;
+    feedback  : ;
+    operation : addPayoffs buyer ;
+    outputs   :  ;
+    returns   :  ;
+
+    :-----:
+
+    outputs   : ;
+    returns   : ;
+|]
+
+
+-- | Insurance in place for a period of time
+insuranceClauseExogenous seller buyer insuranceCostFunction insuranceCondition  amountSeller amountBuyer  deadline  time = [opengame|
+
+    inputs    : ;
+    feedback  : ;
+
+    :-----:
+
+    inputs    : insuranceCondition, amountSeller,amountBuyer, deadline, time ;
+    feedback  : ;
+    operation : forwardFunction $ insuranceCostFunction;
+    outputs   : costsSeller,costsBuyer ;
+    returns   : ;
+
+
+    inputs    : costsSeller;
+    feedback  : ;
+    operation : addPayoffs seller ;
+    outputs   :  ;
+    returns   : ;
+
+
+    inputs    : costsBuyer ;
+    feedback  : ;
+    operation : addPayoffs buyer ;
+    outputs   :  ;
+    returns   :  ;
+
+    :-----:
+
+    outputs   : ;
+    returns   : ;
+|]
+
+-----------------------
+-- 2 Concrete Instances
+-----------------------
+
+-- Concrete insurance clauses (1: most favorable buyer; 3: most favorable seller)
+insurance1 amountSeller amountBuyer deadline time = insuranceClauseExogenous "seller" "buyer" favorability Buyer amountSeller amountBuyer deadline time
+insurance2 amountSeller amountBuyer deadline time = insuranceClauseExogenous "seller" "buyer" favorability Both amountSeller amountBuyer deadline time
+insurance3 amountSeller amountBuyer deadline time = insuranceClauseExogenous "seller" "buyer" favorability Seller amountSeller amountBuyer deadline time
